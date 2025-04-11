c3d341351262e8bfd2d027788ed46250f53d7eba
Daniel Spindler <daniel.spindler@braun-its.de>
2025-04-11 16:22:22 +0200

    Implemented basic configuration and UKI setup

--- install/configuration.sh
+++ install/configuration.sh
@@ -20,6 +20,41 @@ function configure_basics() {
     arch-chroot /mnt locale-gen
 }
 
+function setup_uki() {
+    echo "Setting up UKI"
+
+    echo "rw" > /mnt/etc/kernel/cmdline
+    mkdir -p /mnt/efi/EFI/Linux
+
+    echo "Setting mkinitcpio hooks..."
+    HOOKS="(base systemd autodetect modconf kms keyboard sd-vconsole sd-encrypt block filesystems fsck)"
+    sed -i -e "s/^HOOKS=.*/${HOOKS}/g" /mnt/etc/mkinitcpio.conf
+
+    echo "Enabling UKI"
+    sed -i -e "s/^default_config=/#default_config=/g" /mnt/etc/mkinitcpio.d/linux.preset
+    sed -i -e "s/^default_image=/#default_image=/g" /mnt/etc/mkinitcpio.d/linux.preset
+    sed -i -e "s/^#default_uki=/default_uki=/g" /mnt/etc/mkinitcpio.d/linux.preset
+    sed -i -e "s/^#default_options=/default_options=/g" /mnt/etc/mkinitcpio.d/linux.preset
+
+    sed -i -e "s/^fallback_config=/#fallback_config=/g" /mnt/etc/mkinitcpio.d/linux.preset
+    sed -i -e "s/^fallback_image=/#fallback_image=/g" /mnt/etc/mkinitcpio.d/linux.preset
+    sed -i -e "s/^#fallback_uki=/fallback_uki=/g" /mnt/etc/mkinitcpio.d/linux.preset
+    sed -i -e "s/^#fallback_options=/fallback_options=/g" /mnt/etc/mkinitcpio.d/linux.preset
+
+    echo "Generating UKI..."
+    arch-chroot /mnt mkinitcpio -P
+
+    echo "Installing bootloader..."
+    arch-chroot /mnt bootctl install --esp-path=/efi
+
+    sync
+
+    echo "Rebooting, please set Secure Boot in BIOS to setup mode!"
+    read -p "Press any key to reboot and continue" IGNORE
+
+    systemctl reboot --firmware-setup
+}
+
 $LOGIN_NAME=$1
 $HOSTNAME=$2
 
