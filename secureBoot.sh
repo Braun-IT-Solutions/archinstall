441f7a461566eedeb3985d707c032c17033e6e59
Pascal_Brus <email@example.com>
2025-04-14 15:50:46 +0200

    added sudo?

--- secureBoot.sh
+++ secureBoot.sh
@@ -10,13 +10,13 @@ function createKeysAndSign(){
   sbctl enroll-keys -m
 
   echo "Signing Keys.." > /dev/tty
-  sbctl sign -s -o /usr/lib/systemd/boot/efi/systemd-bootx64.efi.signed
-  sbctl sign -s /efi/EFI/BOOT/BOOTX64.efi
-  sbctl sign -s /efi/EFI/Linux/arch-linux.efi
-  sbctl sign -s /efi/EFI/Linux/arch-linux-fallback.efi
+  sudo sbctl sign -s -o /usr/lib/systemd/boot/efi/systemd-bootx64.efi.signed
+  sudo sbctl sign -s /efi/EFI/BOOT/BOOTX64.efi
+  sudo sbctl sign -s /efi/EFI/Linux/arch-linux.efi
+  sudo sbctl sign -s /efi/EFI/Linux/arch-linux-fallback.efi
 
   echo "UKI's neu generieren" > /dev/tty
-  mkinitcpio -P 
+  sudo mkinitcpio -P 
 }
 
 set -eo pipefail
