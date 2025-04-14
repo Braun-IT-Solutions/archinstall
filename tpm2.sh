32221f5ff9720087c99b918ace041137273a49c8
Pascal_Brus <email@example.com>
2025-04-14 15:58:21 +0200

    fix

--- tpm2.sh
+++ tpm2.sh
@@ -1,6 +1,9 @@
+#!/usr/bin/bash
+
+
 function recoveryKey {
   systemd-cryptenroll /dev/gpt-auto-root-luks --recovery-key > recovery_key.txt
   systemd-cryptenroll --tpm2-device=auto --wipe-slot=tpm2 --tpm2-pcrs=0+7 /dev/gtp-auto-root-luks
   systemd-cryptenroll /dev/gpt-auto-root-luks --wipe-slot=password
-  
+
 }
