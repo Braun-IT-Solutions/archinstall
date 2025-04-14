4326c31a4b233019bf6045d6f7d5dd4bef90b181
Pascal_Brus <email@example.com>
2025-04-14 15:45:41 +0200

    added some echos

--- secureBoot.sh
+++ secureBoot.sh
@@ -5,14 +5,17 @@ function checkSetupMode(){
 }
 
 function createKeysAndSign(){
+  echo "Creating keys..." > /dev/tty
   sbctl create-keys
   sbctl enroll-keys -m
 
+  echo "Signing Keys.." > /dev/tty
   sbctl sign -s -o /usr/lib/systemd/boot/efi/systemd-bootx64.efi.signed
   sbctl sign -s /efi/EFI/BOOT/BOOTX64.efi
   sbctl sign -s /efi/EFI/Linux/arch-linux.efi
   sbctl sign -s /efi/EFI/Linux/arch-linux-fallback.efi
 
+  echo "UKI's neu generieren" > /dev/tty
   mkinitcpio -P 
 }
 
