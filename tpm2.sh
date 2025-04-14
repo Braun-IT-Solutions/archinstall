69378b4d3e1e6d9955fbd7fdd064b35054e97b35
Pascal_Brus <email@example.com>
2025-04-14 16:20:41 +0200

    added soome echos

--- tpm2.sh
+++ tpm2.sh
@@ -2,10 +2,17 @@
 
 
 function recoveryKey {
+
+  echo "Recovery Key generieren..." > /dev/tty
   systemd-cryptenroll /dev/gpt-auto-root-luks --recovery-key > recovery_key.txt
+
+  echo "TPM2 ausrollen..." > /dev/tty
   systemd-cryptenroll --tpm2-device=auto --wipe-slot=tpm2 --tpm2-pcrs=0+7 /dev/gpt-auto-root-luks
+
+  echo "Delete Initial Password..." > /dev/tty
   systemd-cryptenroll /dev/gpt-auto-root-luks --wipe-slot=password
 
 }
+set -eo pipefail
 
 recoveryKey
