60dcaafb536f9630b8b20f12fb74c1d07a86a8b9
Pascal_Brus <email@example.com>
2025-04-14 16:22:07 +0200

    removed pass removal

--- tpm2.sh
+++ tpm2.sh
@@ -10,7 +10,7 @@ function recoveryKey {
   systemd-cryptenroll --tpm2-device=auto --wipe-slot=tpm2 --tpm2-pcrs=0+7 /dev/gpt-auto-root-luks
 
   echo "Delete Initial Password..." > /dev/tty
-  systemd-cryptenroll /dev/gpt-auto-root-luks --wipe-slot=password
+  #systemd-cryptenroll /dev/gpt-auto-root-luks --wipe-slot=password
 
 }
 set -eo pipefail
