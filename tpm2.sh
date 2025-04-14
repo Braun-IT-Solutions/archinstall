ba8fcdff12a4b602f55ddb4bebd08c321db2242b
Pascal_Brus <email@example.com>
2025-04-14 16:02:52 +0200

    fix typo

--- tpm2.sh
+++ tpm2.sh
@@ -3,7 +3,7 @@
 
 function recoveryKey {
   systemd-cryptenroll /dev/gpt-auto-root-luks --recovery-key > recovery_key.txt
-  systemd-cryptenroll --tpm2-device=auto --wipe-slot=tpm2 --tpm2-pcrs=0+7 /dev/gtp-auto-root-luks
+  systemd-cryptenroll --tpm2-device=auto --wipe-slot=tpm2 --tpm2-pcrs=0+7 /dev/gpt-auto-root-luks
   systemd-cryptenroll /dev/gpt-auto-root-luks --wipe-slot=password
 
 }
