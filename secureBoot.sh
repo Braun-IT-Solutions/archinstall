0e1ef1aa9751d3279cce0180178373490973985d
Pascal_Brus <email@example.com>
2025-04-15 16:26:25 +0200

    sudo

--- secureBoot.sh
+++ secureBoot.sh
@@ -6,14 +6,14 @@ function checkSetupMode(){
 
 function createKeysAndSign(){
   echo "Creating keys..."
-  sbctl create-keys
-  sbctl enroll-keys -m
+  sudo sbctl create-keys
+  sudo sbctl enroll-keys -m
 
   echo "Signing Keys.."
-  sbctl sign -s -o /usr/lib/systemd/boot/efi/systemd-bootx64.efi.signed /usr/lib/systemd/boot/efi/systemd-bootx64.efi
-  sbctl sign -s /efi/EFI/BOOT/BOOTX64.efi
-  sbctl sign -s /efi/EFI/Linux/arch-linux.efi
-  sbctl sign -s /efi/EFI/Linux/arch-linux-fallback.efi
+  sudo sbctl sign -s -o /usr/lib/systemd/boot/efi/systemd-bootx64.efi.signed /usr/lib/systemd/boot/efi/systemd-bootx64.efi
+  sudo sbctl sign -s /efi/EFI/BOOT/BOOTX64.efi
+  sudo sbctl sign -s /efi/EFI/Linux/arch-linux.efi
+  sudo sbctl sign -s /efi/EFI/Linux/arch-linux-fallback.efi
 
   echo "UKI's neu generieren"
   mkinitcpio -P 
@@ -22,11 +22,11 @@ function createKeysAndSign(){
 function recoveryKey {
 
   echo "Recovery Key generieren..."
-  systemd-cryptenroll /dev/gpt-auto-root-luks --unlock-key-file=luks-temp.key --recovery-key > /home/pascal.brus/recovery_key.txt
+  sudo systemd-cryptenroll /dev/gpt-auto-root-luks --unlock-key-file=luks-temp.key --recovery-key > /home/pascal.brus/recovery_key.txt
 
 
   echo "Rebooting, to setup TPM2 correctly"
-  #systemctl reboot
+  #sudo systemctl reboot
 
 }
 
