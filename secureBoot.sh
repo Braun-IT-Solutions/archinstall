ade174db2ce1d771da3745060780a09c3418e434
Pascal_Brus <email@example.com>
2025-04-15 09:06:33 +0200

    couple things to test

--- secureBoot.sh
+++ secureBoot.sh
@@ -10,22 +10,23 @@ function createKeysAndSign(){
   sbctl enroll-keys -m
 
   echo "Signing Keys.." > /dev/tty
-  sudo sbctl sign -s -o /usr/lib/systemd/boot/efi/systemd-bootx64.efi.signed /usr/lib/systemd/boot/efi/systemd-bootx64.efi
-  sudo sbctl sign -s /efi/EFI/BOOT/BOOTX64.efi
-  sudo sbctl sign -s /efi/EFI/Linux/arch-linux.efi
-  sudo sbctl sign -s /efi/EFI/Linux/arch-linux-fallback.efi
+  sbctl sign -s -o /usr/lib/systemd/boot/efi/systemd-bootx64.efi.signed /usr/lib/systemd/boot/efi/systemd-bootx64.efi
+  sbctl sign -s /efi/EFI/BOOT/BOOTX64.efi
+  sbctl sign -s /efi/EFI/Linux/arch-linux.efi
+  sbctl sign -s /efi/EFI/Linux/arch-linux-fallback.efi
 
   echo "UKI's neu generieren" > /dev/tty
-  sudo mkinitcpio -P 
+  mkinitcpio -P 
 }
 
 function recoveryKey {
 
   echo "Recovery Key generieren..." > /dev/tty
-  sudo systemd-cryptenroll /dev/gpt-auto-root-luks --recovery-key > ~/recovery_key.txt
+  systemd-cryptenroll /dev/gpt-auto-root-luks --recovery-key > ~/recovery_key.txt
 
   echo "TPM2 ausrollen..." > /dev/tty
-  sudo systemd-cryptenroll --tpm2-device=auto --wipe-slot=tpm2 --tpm2-pcrs=0+7 /dev/gpt-auto-root-luks
+  #sleep 5
+  systemd-cryptenroll --tpm2-device=auto --wipe-slot=tpm2 --tpm2-pcrs=0+7 /dev/gpt-auto-root-luks
 
   echo "Delete Initial Password..." > /dev/tty
   #systemd-cryptenroll /dev/gpt-auto-root-luks --wipe-slot=password
